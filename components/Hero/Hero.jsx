3236adb0608bf433c99f220514da01c9662809e7
Danila <daysprng@gmail.com>
2024-06-05 18:44:31 +0300

    hero services cards

--- components/Hero/Hero.jsx
+++ components/Hero/Hero.jsx
@@ -6,9 +6,37 @@ const Hero = () => {
     return (
         <div className={styles.block}>
             <div className={styles.wrapper}>
-            <div className={styles.content}> 
-                <h1 className={styles.heading}>Комплексное обслуживание IT-инфраструктуры вашего бизнеса</h1>
-                <button className={styles.button}>Оставить заявку</button>
+            <div className={styles.content}>
+                <div className={styles.cta}>
+                    <h1 className={styles.heading}>Комплексное обслуживание IT-инфраструктуры вашего бизнеса</h1>
+                    <button className={styles.button}>Оставить заявку</button>
+                </div> 
+                
+                
+                <div className={styles.services}>
+                    <div className={styles.service}>
+                        <div className={styles.service_front}>
+                            <div className={styles.service_icon}><Image src="/images/printer.svg" width={60} height={60} alt="printer" /></div>
+                            <div> Печатная техника</div>
+                        </div>
+                        <div className={styles.service_reverse}>
+                            <div>Поставка печатного оборудования любого класса</div>
+                            <button className={styles.button}>Подробнее</button>
+                        </div>
+                    </div>
+                    <div className={styles.service}>
+                    <div className={styles.service_icon}><Image src="/images/toner.svg" width={60} height={60} alt="toner" /></div>
+                        <div> Картриджи</div>
+                    </div>
+                    <div className={styles.service}>
+                        <div className={styles.service_icon}><Image src="/images/service.svg" width={60} height={60} alt="service" /></div>
+                        <div>Сервис и обслуживание</div>
+                    </div>
+                    <div className={styles.service}>
+                    <div className={styles.service_icon}><Image src="/images/it.svg" width={50} height={60} alt="it" /></div>
+                        <div>IT аутсорсинг</div>
+                    </div>
+                </div>
                 </div>
                 
                 {/* <Image className={styles.image} src="/images/hero.png" alt="Pro System office" quality={100} fill objectFit="cover"/> */}
